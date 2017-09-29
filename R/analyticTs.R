3aef3bbad27a8760b7a4439da42094fc84c4d952
kcucchi <karina.cucchi@gmail.com>
2017-09-29 12:27:13 -0700

    add time field to calculateSyntheticTs output

--- R/analyticTs.R
+++ R/analyticTs.R
@@ -3,8 +3,8 @@
 #' Applies the analytical formula to calculate temperature at one depth, time combination
 #'
 #' @param param the vector of parameters
-#' @param t time at which to calculate the temperature
-#' @param z a vector of depths at which to calculate the temperature.
+#' @param t (in s) time at which to calculate the temperature
+#' @param z (in m) a vector of depths at which to calculate the temperature
 #' Depths need to be between 0 and maximum depth.
 #' @return temperature at depth z and time t for parameters param
 #' @export
@@ -25,7 +25,7 @@ calculateSynthetic_timeDepth <- function(param,t,z){
 
   # areally averaged rate of heat movement
   v_t <-  as.numeric(q * param['rho_w'] * param['c_w'] / rho_mTimesc_m)
-  omega <- 2 * pi / param['period']
+  omega <- 2 * pi / param['period_T']
   alpha <- as.numeric(sqrt ( v_t^4 + (4 * omega * param_eq['kappa_e'])^2 ))
 
   a = 1/(2*param_eq['kappa_e']) * (sqrt((alpha + v_t^2)/2) - v_t)
@@ -50,21 +50,59 @@ calculateSynthetic_timeDepth <- function(param,t,z){
 #' Applies the analytical formula to calculate temperature time series at one depth
 #'
 #' @param param the vector of parameters
-#' @param z depth at which to calculate the temperature
-#' @param nbDays length of output time series, in days
-#' @param Deltat time resolution of output time series
+#' @param z (in m) a vector of depths at which to calculate the temperature
+#' @param nbDays (in days) length of output time series
+#' @param Deltat (in s) time resolution of output time series
 #' @return temperature time series at depth z
 #' @export
-calculateSyntheticTs <- function(param,z,nbDays=3,Deltat=15*60){
+calculateSyntheticTs <- function(param,z,nbDays=3){
 
-  t_seconds = seq(from=0,
-                  to=nbDays*param['period'],
-                  by=Deltat)
+  # generate vector of time values
+  # starts on 01/01/2010
+  # default timezone is "GMT", which in R means "UTC" for some reason
+  # UTC does not have daylight saving times, that's what we want
+  df_t <- data.frame(
+    t_time=seq.POSIXt(from = ISOdate(year = 2000,month = 01,day = 01, hour = 0),
+                      to = ISOdate(year = 2000,month = 01,day = 01+nbDays),
+                      by = param['sampling_period']))
 
+  # calculate time vector in seconds
+  df_t$t_seconds <-
+    as.numeric(difftime(
+      time1 = df_t$t_time,
+      time2 = df_t$t_time[1], # time at origin
+      units = "secs"))
 
-  return(calculateSynthetic_timeDepth(param = param,
-                                      t = t_seconds,
-                                      z = z))
+  # call calculateSynthetic_timeDepth to calculate result
+  res <-
+    calculateSynthetic_timeDepth(param = param,
+                                 t = df_t$t_seconds,
+                                 z = z)
+
+  # join POSIXct vector to res dataframe
+  res <- dplyr::left_join(x = res,
+                          y = df_t,
+                          by = c("t"="t_seconds"))
+
+  # add metaparameters for vector of depths
+  # first define name of indices
+  string_z_idx <- paste0('z_',c('top',1:(length(z)-2),'bottom'))
+  # turn the names into a factor
+  factor_z_idx <-
+    factor(x = string_z_idx,
+           levels = string_z_idx)
+  # this dataframe does the correspondance
+  # between the depths and the name of the indices
+  df_depth2idx <-
+    data.frame(z = sort(z,decreasing = T), # vector of depths in decreasing order
+               z_idx = factor_z_idx) # factor of depths indices
+
+  # join to result dataframe
+  res <- dplyr::left_join(x = res,
+                          y = df_depth2idx,
+                          by = "z")
+
+  return(res[,c('t','t_time','z','z_idx','temperature')])
 
 }
 
