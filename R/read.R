2d56cf70d82f1ce2992f83c73e0cad340219c7df
kcucchi <karina.cucchi@gmail.com>
2017-10-16 13:24:09 -0700

    add function reading parameters from csv and saving them in named dataframe

---
+++ R/read.R
@@ -0,0 +1,37 @@
+
+#' Imports vector of parameter from csv file
+#'
+#' @param csv_file path to the csv file
+#' @return the vector of parameters
+#' @export
+csv2param <- function(csv_file){
+
+  #
+  #  read parameters from file
+  #
+
+  df_param <- read.csv(csv_file,
+                       stringsAsFactors = F)
+  # str(df_param)
+
+  #
+  # create named vector from dataframe
+  #
+
+  # this is for an easier way to access variable values
+  param <- df_param$value
+  names(param)<- df_param$notation
+  rm(df_param)
+
+  #
+  # Calculate equivalent parameters
+  #
+
+  param <- HZinv::update_all_eq(param = param)
+
+  # check consistency
+  HZinv::check_consistency(param)
+
+  return(param)
+
+}
