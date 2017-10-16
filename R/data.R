8dec5db88f9e7c4c757adc461950bd385840b555
kcucchi <karina.cucchi@gmail.com>
2017-10-16 13:20:12 -0700

    add documentation for exported variables

---
+++ R/data.R
@@ -0,0 +1,27 @@
+
+# Contains documentation of exported data
+
+
+#' names of physical parameters
+#'
+#' A vector containing the names of physical parameters of interest
+#'
+#' @format a vector of characters
+#' @source self-defined
+"names_phy"
+
+#' names of reduced set of parameters
+#'
+#' A vector containing the names of reduced parameters of interest
+#'
+#' @format a vector of characters
+#' @source self-defined
+"names_red"
+
+#' names of parameters for boundary conditions
+#'
+#' A vector containing the names of boundary conditions parameters
+#'
+#' @format a vector of characters
+#' @source self-defined
+"names_BC"
